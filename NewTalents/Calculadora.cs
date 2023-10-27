04eb69dd81f376b8435a472ec126a5f8c60c66b3
alandalcastagne <alan.dalcastagne17@gmail.com>
2023-10-27 09:36:32 -0300

    First Commit

---
+++ NewTalents/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTalents
+{
+    public class Calculadora
+    {
+        private List<String> listahistorico;
+        private string data;
+
+        public Calculadora(string data, List<string> lista)
+        {
+            listahistorico = new List<string>();
+            this.data = data;
+        }
+
+        public int Somar(int val1, int val2)
+        {
+            int res = val1 + val2;         
+            listahistorico.Insert(0, "Res: " + res + " data: " + data);
+            return res;
+        }
+
+        public int Subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            listahistorico.Insert(0, "Res: " + res + " data: " + data);
+            return res;
+        }
+
+        public int Multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            listahistorico.Insert(0, "Res: " + res + " data: " + data);
+            return res;
+        }
+
+        public int Dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+            listahistorico.Insert(0, "Res: " + res + " data: " + data);
+            return res;
+        }
+
+        public List<string> historico()
+        {
+            listahistorico.RemoveRange(3, listahistorico.Count - 3);
+            return listahistorico;
+        }
+    }
+}
